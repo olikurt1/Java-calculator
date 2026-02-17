8253323cfe01f3bb9123f6bb23096b6781ff300e
olikurt1 <olikurt1@outlook.com>
2026-02-17 13:12:04 +0000

    Setting custom colors for window and labels with panels to provide place for text

---
+++ README.md
@@ -0,0 +1,6 @@
+Setting up a java project creates libraries and binaries folders along with .vscode configurations for project. 
+
+To run a program, such as an app, the java class recursively calls itself. all frame methods are used to create the window for the calculator
+
+Common java theme, when creating a variable/value from an imported library, typical syntax is: 
+dataType variable name = new dataType value; This is a generic blueprint variable which further variables of such data type may need when being applied to use. 
--- calculator/Calculator/src/Calculator.java
+++ calculator/Calculator/src/Calculator.java
@@ -4,6 +4,29 @@ import java.util.Arrays;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 public class Calculator {
-    
+    int boardwidth = 360;
+    int boardheight = 540;
+    //colours of the buttons initialised for the calculator 
+    Color customLightGray = new Color(212, 212, 210);
+    Color customDarkGray = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(255, 149, 0);
+
+    // create a window, JFrame comes from javax.swing.*
+    JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+
+    //class calling itself possibly recursion to keep it running indefinitely? 
+    Calculator(){
+        //makes window possible to view 
+        frame.setVisible(true);
+        //sets frame size to previously given variables
+        frame.setSize(boardwidth, boardheight);
+        frame.setLocationRelativeTo(null); //centers window
+        frame.setResizable(false ); //prevents window to change size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//shuts window when close button pressed.
+        frame.setLayout(new BorderLayout());
+    }
 
 }
