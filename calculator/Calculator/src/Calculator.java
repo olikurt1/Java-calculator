dcc1135624ea55ba6b57f326f0dabce787d09b18
olikurt1 <olikurt1@outlook.com>
2026-02-17 13:19:43 +0000

    Adding Text to labels and labels to panel further to panel to frame but frame covers entire panel

--- calculator/Calculator/src/Calculator.java
+++ calculator/Calculator/src/Calculator.java
@@ -14,8 +14,9 @@ public class Calculator {
 
     // create a window, JFrame comes from javax.swing.*
     JFrame frame = new JFrame("Calculator");
-    JLabel displayLabel = new JLabel();
-    JPanel displayPanel = new JPanel();
+    JLabel displayLabel = new JLabel(); //text will go inside label
+    JPanel displayPanel = new JPanel(); //label will go inside panel
+
 
     //class calling itself possibly recursion to keep it running indefinitely? 
     Calculator(){
@@ -27,6 +28,19 @@ public class Calculator {
         frame.setResizable(false ); //prevents window to change size
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//shuts window when close button pressed.
         frame.setLayout(new BorderLayout());
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);//add labels into panels
+        frame.add(displayPanel);//add panels into the frame 
+
+
     }
 
 }
